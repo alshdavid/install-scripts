c73ef87f6440b2d30012c972e08c3d4b788a3aee
David Alsh <alshdavid@gmail.com>
2025-09-24 18:21:24 +1000

    python install script

--- sh/python.sh
+++ sh/python.sh
@@ -30,9 +30,48 @@ OS=""
 case "$(uname -s)" in
   Darwin) OS="macos";;
   Linux) OS="linux";;
-  MINGW64_NT* | Windows_NT) OS="windows";;
+  MINGW64_NT* | Windows_NT | MSYS_NT*) OS="windows";;
   *) OS="";;
 esac
 
+if [ "$OS" = "" ]; then
+  echo "Unable to determine version"
+  exit 1
+fi
+
 >&2 echo ARCH: $ARCH
 >&2 echo OS: $OS
+
+echo "http://sh.davidalsh.com/versions/python/${OS}-${ARCH}-${VERSION}"
+URL=$(curl -s "http://sh.davidalsh.com/versions/python/${OS}-${ARCH}-${VERSION}" | tr -d " \t\n\r")
+
+if [ "$URL" = "" ]; then
+  echo "Cannot find archive"
+  exit 1
+fi
+
+>&2 echo URL: $URL
+
+test -d $OUT_DIR/python && rm -rf $OUT_DIR/python
+mkdir -p $OUT_DIR/python
+mkdir -p $OUT_DIR/python/tmp/$VERSION
+
+if [ -z "${URL##*.tar.gz}" ]; then
+  curl -s -L --url $URL | tar -xzf - -C $OUT_DIR/python/tmp/$VERSION
+  mv $OUT_DIR/python/tmp/$VERSION/python $OUT_DIR/python/$VERSION
+  rm -rf $OUT_DIR/python/tmp
+fi
+
+if [ "${OS}" = "windows" ]; then
+  echo "export PATH=\"${OUT_DIR}/python/${VERSION}:\$PATH\""
+
+  if [ "$GITHUB_PATH" != "" ]; then
+    echo "${OUT_DIR}/python/${VERSION}" >> $GITHUB_PATH
+  fi
+else
+  echo "export PATH=\"${OUT_DIR}/python/${VERSION}/bin:\$PATH\""
+
+  if [ "$GITHUB_PATH" != "" ]; then
+    echo "${OUT_DIR}/python/${VERSION}/bin" >> $GITHUB_PATH
+  fi
+fi
