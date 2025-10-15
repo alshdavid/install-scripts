8ad2f96c4c51b3a8c088fea3fe03377713bf566b
David Alsh <12656294+alshdavid@users.noreply.github.com>
2025-10-15 17:09:51 +1100

    Create wp.sh

---
+++ sh/wp.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/sh
+set -e
+
+ARCH=""
+case "$(uname -m)" in
+  x86_64 | x86-64 | x64 | amd64) ARCH="amd64";;
+  aarch64 | arm64) ARCH="arm64";;
+  *) ARCH="";;
+esac
+
+OS=""
+case "$(uname -s)" in
+  Darwin) OS="macos";;
+  Linux) OS="linux";;
+  MINGW64_NT* | Windows_NT | MSYS_NT*) OS="windows";;
+  *) OS="";;
+esac
+
+printf "${OS_ARCH}"
